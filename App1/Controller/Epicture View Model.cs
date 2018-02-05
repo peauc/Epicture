38c29675a096bceeb5bd51952b0211e11949fb84
Clément Péau <clement.peau@epitech.eu>
2018-02-05 14:34:25 +0100

    Push

--- App1/Controller/Epicture View Model.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace App1.Controller
-{
-    public class Epicture_View_Model
-    {
-        
-    }
-}
---
+++ App1/Controller/EpictureViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace App1.Controller
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    using App1.Annotations;
+
+    public class EpictureViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
